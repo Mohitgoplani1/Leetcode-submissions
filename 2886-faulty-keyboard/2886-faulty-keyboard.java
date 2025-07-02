07255aef68db240357732544f120b4ad509d7d00
Mohitgoplani1 <147914796+Mohitgoplani1@users.noreply.github.com>
2025-07-02 10:14:02 +0530

    Time: 3 ms (95.19%), Space: 44.8 MB (75.04%) - LeetHub

---
+++ 2886-faulty-keyboard/2886-faulty-keyboard.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String finalString(String s) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if(ch == 'i'){
+                sb.reverse();
+            }else{
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString();
+    }
+}
